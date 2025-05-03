fc3ca26c818e086b80a6ec1f03d42a37fd7a8fb9
Yunus Gedik <yunus-gedik@outlook.com>
2025-05-03 12:42:40 +0300

    feat: complete get requests

--- src/main/java/com/post/comment/example/Controller/PostController.java
+++ src/main/java/com/post/comment/example/Controller/PostController.java
@@ -2,54 +2,63 @@ package com.post.comment.example.Controller;
 
 import com.post.comment.example.Model.Comment;
 import com.post.comment.example.Model.Post;
+import com.post.comment.example.Repository.CommentRepository;
+import com.post.comment.example.Repository.PostRepository;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RequestMapping("/api")
 @RestController
 class PostController {
-//    private final PostRepository repo;
-    PostController(){}
+    private final PostRepository postRepo;
+    private final CommentRepository commentRepo;
+    PostController(PostRepository repo, CommentRepository commentRepo) {
+        this.postRepo = repo;
+        this.commentRepo = commentRepo;
+    }
 
     @GetMapping("/posts")
-    public List<Post> getPosts(@RequestParam(value = "of", required = false) int userId){
-        return null;
+    public List<Post> getPosts(@RequestParam(value = "of", required = false) Long userId) {
+        return this.postRepo.findByUserId(userId);
     }
 
     @GetMapping("/post")
-    public Post getPostByRequestParam(@RequestParam(value = "id") int postId){
-        return null;
+    public Optional<Post> getPostByRequestParam(@RequestParam(value = "id") Long postId) {
+        return postRepo.findById(postId);
     }
 
     @GetMapping("/post/{id}")
-    public Post getPost(@PathVariable(value = "id", required = true) int postId){
-        return null;
+    public Optional<Post> getPost(@PathVariable(value = "id", required = true) long postId) {
+        return postRepo.findById(postId);
     }
 
     @GetMapping("/post/{id}/comments")
-    public List<Comment> getComments(@PathVariable(value = "id") int postId){
-        return null;
+    public List<Comment> getComments(@PathVariable(value = "id") long postId) {
+        return commentRepo.findByPostId(postId);
     }
 
     @GetMapping("/comments")
-    public List<Comment> getCommentsByRequestParam(@RequestParam(value = "of") int postId){
-        return null;
+    public List<Comment> getCommentsByRequestParam(@RequestParam(value = "of", required = true) Long postId) {
+        return commentRepo.findByPostId(postId);
     }
 
+
     @PostMapping("/post")
-    public Post sharePost(@RequestBody Post post){
+    public Post sharePost(@RequestBody Post post) {
         return null;
     }
 
     @PatchMapping("/post/{id}")
-    public Post updatePost(@PathVariable(value = "id") int postId, @RequestBody Post post){
+    public Post updatePost(@PathVariable(value = "id") int postId, @RequestBody Post post) {
         return null;
     }
 
     @PatchMapping("/comment/{id}")
-    public Post updateComment(@PathVariable(value= "id") int commentId, @RequestBody Post post){
+    public Post updateComment(@PathVariable(value = "id") int commentId, @RequestBody Post post) {
         return null;
     }
+
 
 }
